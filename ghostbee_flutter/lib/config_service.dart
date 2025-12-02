3e1b86ba0dc87e4b8dc3f771af6b4612ab939549
THEBLACKDAN <blackdanstar@gmail.com>
2025-12-02 11:02:14 +0700

    merge remote main

---
+++ ghostbee_flutter/lib/config_service.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ConfigService {
+  static String baseUrl = "";
+
+  static Future<void> load() async {
+    final url =
+        "https://raw.githubusercontent.com/THEBLACKDAN/GhostBee_flutter/main/ghostbee_flutter/config.json";
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      baseUrl = jsonData["api_base_url"];
+      print("🔧 Loaded API URL: $baseUrl");
+    } else {
+      throw Exception("❌ Failed to load config.json");
+    }
+  }
+}
--- ghostbee_flutter/lib/constants.dart
+++ ghostbee_flutter/lib/constants.dart
@@ -1,6 +1,7 @@
 // lib/constants.dart
-// const String baseUrl = "https://192.168.1.109/api";
 
-const String baseUrl = "http://192.168.101.89:3000";
+const String baseUrl = "https://yummy-lions-ring.loca.lt";
 // ใช้ Host หลักสำหรับ Socket.io เพื่อให้ Nginx Reverse Proxy ทำงานง่าย
-const String socketHost = "https://192.168.101.89";
+// const String socketHost = "https://192.168.101.89";
+
+
